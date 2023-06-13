b0214b333e7f99e0af00e2015087bfd73d9b0ab2
leokingue <ebangkingue@icloud.com>
2023-06-13 15:52:52 +0100

    Initialize Backend of EMI_PROJECT

---
+++ src/main/java/com/emi/ecommerce/Emi237Application.java
@@ -0,0 +1,13 @@
+package com.emi.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Emi237Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Emi237Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/emi/ecommerce/Emi237ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emi.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Emi237ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
